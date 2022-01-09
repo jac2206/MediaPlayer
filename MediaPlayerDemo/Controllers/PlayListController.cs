38fb1a88bc512bdc6a7853f3f8cb0877e3f057a8
jac2206 <arango773@gmail.com>
2022-01-08 21:24:27 -0500

    Se agrego lista de videos y se coloco parte logica aparte

---
+++ MediaPlayerDemo/Controllers/PlayListController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using MediaPlayerDemo.Models;
+using MediaPlayerDemo.Logic;
+
+namespace MediaPlayerDemo.Controllers
+{
+    public class PlayListController : Controller
+    {
+        // GET: PlayList
+        public ActionResult Index()
+        {
+            return View(VideoLogic.TheStrokesGetVideos());
+        }
+
+        public ActionResult Videos_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(VideoLogic.TheStrokesGetVideos().ToDataSourceResult(request));
+        }
+
+       
+    }
+}
