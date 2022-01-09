22047f56dafb32f09be21cf6932626343935edc2
jac2206 <arango773@gmail.com>
2022-01-09 16:22:39 -0500

    Se agrego controlador y logica para la pantalla de seleccionar lista de reproduccion

---
+++ MediaPlayerDemo/Controllers/SelectedPlayListController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediaPlayerDemo.Controllers
+{
+    public class SelectedPlayListController : Controller
+    {
+        // GET: SelectedPlayList
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult SeleccionarListaReproduccion()
+        {
+            return RedirectToAction("Index", "PlayList");
+
+        }
+
+    }
+}
