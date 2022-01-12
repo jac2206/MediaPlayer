19e4a53a0d998182ac899c9da1f535b33906b746
jac2206 <arango773@gmail.com>
2022-01-11 21:53:05 -0500

    Se agrego controlador y vista para menu lateral

---
+++ MediaPlayerDemo/Controllers/MenuLatetalController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MediaPlayerDemo.Logic;
+using MediaPlayerDemo.Models;
+
+namespace MediaPlayerDemo.Controllers
+{
+    public class MenuLatetalController : Controller
+    {
+        // GET: MenuLatetal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[AcceptVerbs(HttpVerbs.Post)]
+        //public ActionResult MostrarContenido(string nombreContenido)
+        //{
+        //    //Video videosMostrar = VideoLogic.ObtenerVideos("theStrokes").Where(video => video.title = nombreContenido);
+
+        //    //List<Video> videos  = (List<Video>)VideoLogic.ObtenerVideos("theStrokes");
+
+        //    //var videoSeleccinado = from vid in videos
+        //    //                       where vid.source = "sas"
+        //    //                       select vid.
+
+        //    //List<Video> videos = (List<Video>)VideoLogic.ObtenerVideos("theStrokes");
+
+        //    var videoSeleccinado = from vid in VideoLogic.ObtenerVideos("theStrokes")
+        //                           where vid.title.Contains(nombreContenido)
+        //                           select vid;
+
+        //    //IEnumerable<Video> videoSeleccinado = from vid in VideoLogic.ObtenerVideos("theStrokes")
+        //    //                                      where vid.title.Contains(nombreContenido)
+        //    //                                      select vid;            
+
+
+        //    //return Json(new {titulo = "Video", source = "https://www.youtube.com/watch?v=2OGc66e2E9s"}, JsonRequestBehavior.AllowGet);
+        //    return Json(new { titulo = videoSeleccinado.FirstOrDefault().title, source = videoSeleccinado.FirstOrDefault().source }, JsonRequestBehavior.AllowGet);
+
+        //}
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult MostrarContenido(string nombreContenido)
+        {
+           
+            var videoSeleccinado = from vid in VideoLogic.ObtenerVideos("theStrokes")
+                                   where vid.title.Contains(nombreContenido)
+                                   select vid;
+
+            return Json(new { titulo = videoSeleccinado.FirstOrDefault().title, source = videoSeleccinado.FirstOrDefault().source }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
